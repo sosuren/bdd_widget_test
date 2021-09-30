b36bed5c96b8c0f61499742f8e19cd49774e1087
yonjan <surendrayonjan@gmail.com>
2021-09-30 18:06:24 +0545

    test: add table step generation

--- test/step/generic_step_test.dart
+++ test/step/generic_step_test.dart
@@ -125,4 +125,79 @@ Future<void> iInvokeTest(WidgetTester tester) async {
       expectedSteps,
     );
   });
+
+  test('Testing Table Steps', () {
+    const path = 'test';
+    const featureFile = '''
+Feature: Testing feature
+    Scenario: Testing scenario
+        Given I am in the register form
+        When I input these data in register form
+          |  Username  |   IsPresent |
+          |  someuser  |     yes     |
+          |  otheruser |     no      |
+        Then I should see these results in attendance table
+          |  Username  |   IsPresent |
+          |  someuser  |     yes     |
+          |  otheruser |     no      |
+    ''';
+
+    const expectedScenarioStep = '''
+import 'package:flutter_test/flutter_test.dart';
+
+Future<void> testingScenario() async {
+  /// mock here
+  throw UnimplementedError();
+}
+''';
+
+    const expectedStep1 = '''
+import 'package:flutter_test/flutter_test.dart';
+
+Future<void> iAmInTheRegisterForm(WidgetTester tester) async {
+  throw UnimplementedError();
+}
+''';
+
+    const expectedStep2 = '''
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bdd_widget_test/bdd_widget_test.dart';
+
+Future<void> iInputTheseDataInRegisterForm(WidgetTester tester, StepTable table) async {
+  throw UnimplementedError();
+}
+''';
+
+    const expectedStep3 = '''
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bdd_widget_test/bdd_widget_test.dart';
+
+Future<void> iShouldSeeTheseResultsInAttendanceTable(WidgetTester tester, StepTable table) async {
+  throw UnimplementedError();
+}
+''';
+
+    final feature = FeatureFile(
+        featureDir: '$path.feature', tablesFilename: '${path}_tables.feature', package: path, input: featureFile);
+
+    expect(feature.getStepFiles().whereType<NewStepFile>().length, 4);
+
+    expect(
+      feature.getStepFiles().whereType<NewStepFile>().first.dartContent,
+      expectedScenarioStep,
+    );
+
+    expect(
+      feature.getStepFiles().whereType<NewStepFile>().toList()[1].dartContent,
+      expectedStep1,
+    );
+    expect(
+      feature.getStepFiles().whereType<NewStepFile>().toList()[2].dartContent,
+      expectedStep2,
+    );
+    expect(
+      feature.getStepFiles().whereType<NewStepFile>().toList()[3].dartContent,
+      expectedStep3,
+    );
+  });
 }
